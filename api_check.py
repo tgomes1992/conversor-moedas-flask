4a88bd3530ece6b8a90182df4e6f13974cc6403c
Thiago da Conceição Gomes <tt7.thiago@gmail.com>
2023-01-14 23:56:51 -0300

    Pequenas refatorações para atualização do projeto

--- api_check.py
+++ api_check.py
@@ -2,40 +2,53 @@ import requests
 import json
 
 
-def calculo_taxa(tipo,valor):
-    if tipo=="Boleto":
-        return float(valor) * 0.0145
-    elif tipo=="Cartao":
-        return float(valor) * 0.0763
+class ConversorApi:
+    def calculo_taxa(self,tipo,valor):
+        if tipo=="Boleto":
+            return float(valor) * 0.0145
+        elif tipo=="Cartao":
+            return float(valor) * 0.0763
 
 
-def taxaconversao(valor):
-    if float(valor) <= 3000 :
-        return float(valor) * 0.02
-    elif float(valor)>3000 :
-        return float(valor) * 0.01
-    
+    def taxaconversao(self,valor):
+        if float(valor) <= 3000 :
+            return float(valor) * 0.02
+        elif float(valor)>3000 :
+            return float(valor) * 0.01
 
+    def empty_dict(self):
+        resultado = {
+            'origem': 'BRL',
+            'destino': "" ,
+            'valor': f"R$ ",
+            'taxa_compra': "",
+            'pagamento': "",
+            'valor_conversão': "",
+            'taxapagto': "",
+            'taxaconversao': "",
+            'conversao-taxas': ""
+        }
+        return resultado
 
-def conversao(destino,valor,pagamento):
-    busca = f"https://economia.awesomeapi.com.br/last/{destino}-BRL"
-    request_resultado = requests.get(busca).content.decode("utf-8")
-    bid = json.loads(request_resultado)[f'{destino}BRL']['bid']
-    base_conversao = round(float(valor) - taxaconversao(valor) - calculo_taxa(pagamento, valor),2)
+    def conversao(self, destino,valor,pagamento):
+        busca = f"https://economia.awesomeapi.com.br/last/{destino}-BRL"
+        request_resultado = requests.get(busca).content.decode("utf-8")
+        bid = json.loads(request_resultado)[f'{destino}BRL']['bid']
+        base_conversao = round(float(valor) - self.taxaconversao(valor) - self.calculo_taxa(pagamento, valor),2)
 
-    resultado = {
-        'origem': 'BRL',
-        'destino': destino ,
-        'valor': f"R$ {valor}" ,
-        'taxa_compra': bid,
-        'pagamento': "Cartão de Crédito" if pagamento=="Cartao" else pagamento ,
-        'valor_conversão': round(base_conversao/float(bid),2), 
-        'taxapagto': round(calculo_taxa(pagamento, valor),2) ,  
-        'taxaconversao': round(taxaconversao(valor)),
-        'conversao-taxas': base_conversao
-    }
+        resultado = {
+            'origem': 'BRL',
+            'destino': destino ,
+            'valor': f"R$ {valor}",
+            'taxa_compra': bid,
+            'pagamento': "Cartão de Crédito" if pagamento=="Cartao" else pagamento ,
+            'valor_conversão': round(base_conversao/float(bid),2),
+            'taxapagto': round(self.calculo_taxa(pagamento, valor),2) ,
+            'taxaconversao': round(self.taxaconversao(valor)),
+            'conversao-taxas': base_conversao
+        }
 
-    return (resultado)
+        return resultado
 
 
 
--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template ,redirect , url_for ,request
-from api_check import conversao
+from api_check import ConversorApi
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',  methods=["GET",'POST'])
 def main():
-    return render_template('main.html')
+    api = ConversorApi()
+    if request.method =="POST":
+        consulta = api.conversao(request.form['destino'], request.form['conversao'], request.form['tipoPagamento'])
+    else:
+        consulta = api.empty_dict()
+    return render_template('main.html' , resposta=consulta)
 
 
 @app.route("/get_api",methods=['POST'])
 def get_result():
-    consulta = conversao(request.form['destino'], request.form['conversao'], request.form['tipoPagamento'])
+    api = ConversorApi()
+    consulta = api.conversao(request.form['destino'], request.form['conversao'], request.form['tipoPagamento'])
     return render_template('resultado.html',resposta=consulta)
 
 
